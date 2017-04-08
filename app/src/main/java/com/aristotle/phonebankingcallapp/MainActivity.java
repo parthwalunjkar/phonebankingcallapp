a55e79efae6983af3655e60796fede92e3465189
Smriti <rsmritimurty@gmail.com>
2017-04-08 15:35:48 +0530

    Added OpenURL

--- app/src/main/java/com/aristotle/phonebankingcallapp/MainActivity.java
+++ app/src/main/java/com/aristotle/phonebankingcallapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.aristotle.phonebankingcallapp;
 
 
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.R.attr.duration;
+import static com.aristotle.phonebankingcallapp.R.id.text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -48,26 +55,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });}
 
-        public static String login(EditText emailID, EditText password){
+        public  String login(EditText emailID, EditText password){
 
- //       URL url;
-        HttpURLConnection urlConnection = null;
-            HttpClient Client = new DefaultHttpClient();
-        try {
-            String url = "https://corporateapiprojectwar.mybluemix.net/corporate_banking/mybank/authenticate_client?client_id=rsmritimurty@gmail.com&password=3E6HU5RP";
+     OpenURL openurl = new OpenURL();
+            openurl.execute();
 
-        String setServerString;
-
-            HttpGet httpget=new HttpGet(url);
-            ResponseHandler<String>responseHandler= new BasicResponseHandler();
-            setServerString=Client.execute(httpget,responseHandler);
-
-            System.out.println(setServerString);
-
-        }
-            catch(IOException e){
-                e.printStackTrace();
-        }
 
     return "Parth";
 
