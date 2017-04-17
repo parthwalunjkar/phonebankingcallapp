e6e7855fb490506d723fc3c19b0dcf97f0039313
Smriti <rsmritimurty@gmail.com>
2017-04-17 23:12:47 +0530

    View profile page update

--- app/src/main/java/com/aristotle/phonebankingcallapp/ViewProfile.java
+++ app/src/main/java/com/aristotle/phonebankingcallapp/ViewProfile.java
@@ -1,8 +1,18 @@
 package com.aristotle.phonebankingcallapp;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -20,19 +30,20 @@ public class ViewProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
         OpenViewProfileURL openurl = new OpenViewProfileURL();
-        TextView name= (TextView) findViewById(R.id.txtName);
-        TextView docNum= (TextView) findViewById(R.id.txtDocNum);
-        TextView docType= (TextView)findViewById(R.id.txtDocType);
-        TextView kycType= (TextView)findViewById(R.id.txtKYCType);
-        TextView expiry= (TextView)findViewById(R.id.txtExpiry);
+        TextView name = (TextView) findViewById(R.id.txtName);
+        TextView docNum = (TextView) findViewById(R.id.txtDocNum);
+        TextView docType = (TextView) findViewById(R.id.txtDocType);
+        TextView kycType = (TextView) findViewById(R.id.txtKYCType);
+        TextView expiry = (TextView) findViewById(R.id.txtExpiry);
+
+
 
         try {
 
-            String status=openurl.execute().get().toString();
+            String status = openurl.execute().get().toString();
             System.out.println("All Done");
-            if (status.equals("true"))
-            {
-                setTitle("Details for "+MainActivity.name);
+            if (status.equals("true")) {
+                setTitle("Details for " + MainActivity.name);
                 name.setText(OpenViewProfileURL.name);
                 //System.out.println(name);
                 docNum.setText(OpenViewProfileURL.docNum);
@@ -40,11 +51,42 @@ public class ViewProfile extends AppCompatActivity {
                 kycType.setText(OpenViewProfileURL.kycType);
                 expiry.setText(OpenViewProfileURL.expiryDate);
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        }
+
+        Button btnCallToKyc = (Button) findViewById(R.id.btncalltokyc);
+        btnCallToKyc.setOnClickListener((view) -> {
+            Builder dialogBuilder=new Builder(ViewProfile.this);
+            dialogBuilder.setCancelable(false);
+            dialogBuilder.setTitle("Call Phone Banking");
+            dialogBuilder.setMessage("Do you want to call for saving account enquiries?");
+            dialogBuilder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText  (ViewProfile.this, "Calling", Toast.LENGTH_SHORT).show();
+                    String phNo = "tel:+91-904-965-3059";
+                   Intent phoneIntent = new Intent( Intent.ACTION_CALL );
+                    phoneIntent.setData(Uri.parse(phNo));
+                    if (ActivityCompat.checkSelfPermission( ViewProfile.this,
+                            Manifest.permission.CALL_PHONE ) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    startActivity( phoneIntent );
 
 
+                }
+            });
+            dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+
+            });
+            AlertDialog alert=dialogBuilder.create();
+            alert.show();
+        });
+
+    }
     }
